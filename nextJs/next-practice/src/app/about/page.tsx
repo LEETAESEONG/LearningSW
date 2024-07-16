ab7dfc6d98de3da3632f850063adda102c9a672a
이태성 <ga1754@naver.com>
2024-07-17 00:58:01 +0900

    [study] next.js 17, 18

--- nextJs/next-practice/src/app/about/page.tsx
+++ nextJs/next-practice/src/app/about/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "About Absolute",
+    default: "About Codevolution",
+    template: "",
+  },
+};
+
 export default function About() {
   return <h1>About me</h1>;
 }
--- nextJs/next-practice/src/app/products/[productId]/page.tsx
+++ nextJs/next-practice/src/app/products/[productId]/page.tsx
@@ -1,3 +1,32 @@
+import { Metadata } from "next";
+
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+// 비동기 함수로 정의하기
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
 export default function ProductDetails({
   params,
 }: {
