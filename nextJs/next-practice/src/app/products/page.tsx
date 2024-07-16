a2c111c1b07d660d5557b0448e828997dc8a9b09
이태성 <ga1754@naver.com>
2024-07-17 01:38:23 +0900

    [study] next.js 19, 20

--- nextJs/next-practice/src/app/products/page.tsx
+++ nextJs/next-practice/src/app/products/page.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
+
 export default function ProductList() {
+  const product100 = 100;
   return (
     <>
+      <Link href={"/"}>Home</Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href={"/products/1"}>Product 1</Link>
+      </h2>
+      <h2>
+        <Link href={"/products/2"}>Product 2</Link>
+      </h2>
+      <h2>
+        <Link href={"/products/3"}>Product 3</Link>
+      </h2>
+      <h2>
+        <Link href={`/products/${product100}`} replace>
+          product100
+        </Link>
+      </h2>
     </>
   );
 }
